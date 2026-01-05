b6ad81197ec935b83ab514d6b2fda28677b8e76a
DanielDamasc <dandamasceno04@gmail.com>
2026-01-04 21:11:39 -0300

    Criando model e migration de Clients

---
+++ MjEngenharia/database/migrations/2026_01_04_235824_create-clients-table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('clients', function(Blueprint $table) {
+            $table->id();
+            $table->string('nome');
+            $table->string('telefone', 11); // Tamanho padrão de 11 para telefone
+            $table->string('email')->nullable(); // Email pode ser nulo
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('clients');
+    }
+};
