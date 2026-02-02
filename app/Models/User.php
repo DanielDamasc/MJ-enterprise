840ffe2d800fed53097c028909326fea93c5c705
Daniel Damasceno <dandamasceno04@gmail.com>
2026-02-01 23:34:40 -0300

    refactor: Usando o resend para o envio de email de redefinição de senha

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,8 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use App\Notifications\ResetPasswordQueued;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -57,6 +59,12 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function sendPasswordResetNotification($token)
+    {
+        // Chamada da classe que lida com a notificação de redefinição de senha.
+        $this->notify(new ResetPasswordQueued($token));
     }
 
     public function servicos()
---
+++ app/Notifications/ResetPasswordQueued.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class ResetPasswordQueued extends Notification implements ShouldQueue
+{
+    use Queueable;
+
+    public $token;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct($token)
+    {
+        $this->token = $token;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): MailMessage
+    {
+        $url = url(route('password.reset', [
+            'token' => $this->token,
+            'email' => $notifiable->getEmailForPasswordReset(),
+        ], false));
+
+        return (new MailMessage)
+            ->subject('Redefinição de senha - MJ Engenharia')
+            ->greeting('Olá!')
+            ->line('Você recebeu este e-mail porque foi solicitada uma redefinição de senha para sua conta.')
+            ->action('Redefinir Minha Senha', $url)
+            ->line('Se você não solicitou essa redefinição, nenhuma ação é necessária.')
+            ->salutation('Atenciosamente, Equipe MJ');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            //
+        ];
+    }
+}
